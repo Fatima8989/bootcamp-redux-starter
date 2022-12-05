1f9b17b1169aa6bf5949e1850a14d9f7f3836e31
Fatima8989 <fatimapoladli45@gmail.com>
2022-12-05 15:03:46 +0400

    add space

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -6,10 +6,10 @@ const initialState={
         title: "Logitech C920 Hd Pro Webcam",
         description: "Full HD 1080p video that's faster, smoother and works on more computers. Skype in Full HD 1080p Get breathtaking Full HD 1080p video calls on Skype for the sharpest video-calling experience. Smoother. Sharper. Richer. Clearer. Logitech Fluid Crystal Technology. It's what makes a Logitech webcam better.",
         price: 120
-    },
+    }, 
     {
         id: 1,
-        title: "Logitech USB Headset H390 with Noise Cancelling Mic",
+        title: "Logitech US B Headset H390 with Noise Cancelling Mic",
         description: "Padded headband and ear pads. Frequency response (Microphone): 100 hertz - 10 kilohertz Rotating, noise canceling microphone. Sensitivity (headphone) 94 dBV/Pa +/ 3 dB. Sensitivity (microphone) 17 dBV/Pa +/ 4 dB",
         price: 50
     },
